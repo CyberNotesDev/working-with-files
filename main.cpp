8197baf532f07821748f9a999527407a348db541
Elijah Littell <elittell214@gmail.com>
2021-03-24 18:15:23 -0400

    Added comments and fstream library

--- main.cpp
+++ main.cpp
@@ -1,4 +1,10 @@
 #include <iostream>
+// To work with files in C++ you have to add the following on line 7
+// In this library (fstream) it has 3 classes
+// ofstream --- Creates and writes to files
+// ifstream ---	Reads from files
+// fstream --- 	A combination of ofstream and ifstream: creates, reads, and writes to files
+#include <fstream>
 
 int main() {
     return 0;
